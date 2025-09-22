1acff23ea99b432ca4db554b418f707bfbedd0c3
Alex <castanedajefferson05@gmail.com>
2025-09-22 10:15:57 -0500

    Update matricula.blade.php

--- resources/views/matricula/matricula.blade.php
+++ resources/views/matricula/matricula.blade.php
@@ -2,12 +2,6 @@
 @section('title', 'Matricula')
 @section('content')
 
-  <header>
-    <a href="../matriculapag.html" class="logo" aria-label="Logo Escuela de Fútbol Pinze">
-      <img src="../imagenes/logo.jpg" alt="Logo Escuela de Fútbol Pinze" />
-    </a>
-    <a href="../matriculapag.html" class="volver">Volver</a>
-  </header>
 
   <div class="login-box" role="main" aria-label="Formulario de matrícula">
     <h1>Matrícula</h1>
