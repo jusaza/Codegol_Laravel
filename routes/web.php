b1164ffd5363475ebdc72d8d4e41d9fc58521fd9
Alex <castanedajefferson05@gmail.com>
2025-09-24 11:35:15 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,4 @@ Route::resource('matriculas1',MatriculaController::class);
 Route::get('/matriculapag', [App\Http\Controllers\MatriculaController::class, 'pag'])->name('matriculapag');
 
 Route::resource('rendimientos', RendimientoController::class);
+Route::get('/rendimientopag', [App\Http\Controllers\RendimientoController::class, 'index'])->name('rendimientopag');
