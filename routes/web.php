89c0721c872a96b7e9b4dfe7d3d7f5a4a483cb65
jusaza_oficial <almanzasasajuliandavid4@gmail.com>
2025-09-24 21:46:52 -0500

    sin cambios

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EntrenamientoController;
-use App\Http\Controllers\InventarioController;
-use App\Http\Controllers\UsuarioController;
-use App\Http\Controllers\MatriculaController;
-use App\Http\Controllers\PagoController;
-use App\Http\Controllers\RendimientoController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
-Route::view('/servicios', 'servicios');
-Route::view('/nosotros', 'nosotros');
-Route::view('/login', 'auth.login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Rutas para las subcarpetas
-Route::view('/entrenamiento', 'entrenamiento.entrenamiento');
-Route::view('/entrenamientopag', 'entrenamiento.entrenopag');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::view('/inventario', 'inventario.inventarios');
-Route::view('/inventariopag', 'inventario.inventariopag');
-
-Route::view('/matricula', 'matricula.matricula');
-Route::view('/matriculapag', 'matricula.matriculapag');
-
-Route::view('/rendimiento', 'rendimiento.rendimiento');//loco
-Route::view('/rendimientopag', 'rendimiento.rendimientopag');//locos
-
-Route::view('/usuario/actregistro', 'usuario.actregristro');
-Route::view('/usuario/registropag', 'usuario.registropag');
-Route::view('/pagina_original', 'pagina_original');
-
-
-//Controladores 
-
-Route::resource('usuarios', UsuarioController::class);
-Route::resource('pago', PagoController::class);
-
-Route::resource('entrenamientos', EntrenamientoController::class);
-Route::get('/entrenamientopag', [EntrenamientoController::class, 'index'])->name('entrenamientos.page');
-
-Route::resource('inventarios', InventarioController::class);
-Route::get('/inventariopag', [App\Http\Controllers\InventarioController::class, 'index'])->name('inventariopag');
-
-Route::resource('matriculas1',MatriculaController::class);
-Route::get('/matriculapag', [App\Http\Controllers\MatriculaController::class, 'pag'])->name('matriculapag');
-
-Route::resource('rendimientos', RendimientoController::class);//locos3
-Route::get('/rendimientopag', [App\Http\Controllers\RendimientoController::class, 'index'])->name('rendimientopag');//locos4
+require __DIR__.'/auth.php';
